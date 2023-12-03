830f5191e8055cf1f225dbdc7a338a6a79c64862
Maria Gorelik <marya.gorelik@gmail.com>
2023-12-03 20:13:36 +0300

    first commit

---
+++ Примеры/01/rmiBank/Doctor.java
@@ -0,0 +1,149 @@
+package rmiDoctor;
+
+import java.rmi.*;
+import java.util.List;
+
+/**
+ * This class is a placeholder that simply contains other classes and 
+ * for interfaces remote banking.
+ **/
+public class Doctor {
+    /**
+     * This is the interface that defines the exported methods of the 
+     * bank server.
+     **/
+    public interface RemoteDoctor extends Remote {
+        /** Open a new account, with the specified name and password */
+        public void connect(String surname, String name, String patronymic)
+	    throws RemoteException, RecordException;
+	
+        /** Close the named account */
+        void disconnect(String surname, String name, String patronymic)
+	    throws RemoteException, RecordException;
+	
+        /** Deposit money into the named account */
+        public void record(String surname, String name, String patronymic, String day,
+                           String time, String phone, String complaint)
+	    throws RemoteException, RecordException;
+
+        String options()
+        throws RemoteException, RecordException;
+
+//        /** Withdraw the specified amount of money from the named account */
+//        public FunnyMoney withdraw(String name, String password, int amount)
+//	    throws RemoteException, BankingException;
+	
+        /** Return the amount of money in the named account */
+        public String checkRecord(String surname, String name, String patronymic)
+	    throws RemoteException, RecordException;
+	
+        /** 
+	 * Return a List of Strings  list the transaction history
+	 * of the named account 
+	 **/
+//        public List<String> getTransactionHistory(String name, String password)
+//	    throws RemoteException, BankingException;
+    }
+    
+    /**
+     * This simple class represents a monetary amount.  This implementation
+     * is really nothing more than a wrapper around an integer.  It is a useful
+     * to demonstrate that RMI can accept arbitrary non-String objects as
+     * arguments and return them as values, as long as they are Serializable.
+     * A more complete implementation of this FunnyMoney class might bear
+     * a serial number, a digital signature, and other security features to 
+     * ensure that it is unique and non-forgeable.
+     **/
+//    public static class FunnyMoney implements java.io.Serializable {
+//		/**
+//		 *
+//		 */
+//		private static final long serialVersionUID = 1L;
+//		public int amount;
+//        public FunnyMoney(int amount) { this.amount = amount; }
+//    }
+    
+    /**
+     * This is a type of exception used to represent exceptional conditions
+     * related to banking, such as "Insufficient Funds" and  "Invalid Password"
+     **/
+    public static class RecordException extends Exception {
+		private static final long serialVersionUID = 1L;
+		public RecordException(String msg) { super(msg); }
+    }
+    
+    /**
+     * This class is a simple stand-alone client program that interacts
+     * with a RemoteBank server.  It invokes different RemoteBank methods
+     * depending on its command-line arguments, and demonstrates just how
+     * simple it is to interact with a server using RMI.
+     **/
+    public static class Client {
+        public static void main(String[] args) {
+            try {
+                // Figure out what RemoteBank to connect to by reading a system
+                // property (specified on the command line with a -D option to
+                // java) or, if it is not defined, use a default URL.  Note
+                // that by default this client tries to connect to a server on
+                // the local machine
+                String url = System.getProperty("doctor", "rmi:///FirstRemote");
+		
+                // Now look up that RemoteBank server using the Naming object,
+                // which contacts the rmiregistry server.  Given the url, this
+                // call returns a RemoteBank object whose methods may be
+                // invoked remotely
+                RemoteDoctor doctor = (RemoteDoctor) Naming.lookup(url);
+                
+                // Convert the user's command to lower case
+                String cmd = args[0].toLowerCase();
+		
+                // Now, go test the command against a bunch of possible options
+                if (cmd.equals("connect")) {           // Open an account
+                    doctor.connect(args[1], args[2], args[3]);
+                    System.out.println("User connected.");
+                }
+                else if (cmd.equals("disconnect")) {     // Close an account
+                    doctor.disconnect(args[1], args[2], args[3]);
+                    System.out.println("Goodbye!");
+                } else if (cmd.equals("options"))
+                {
+                    String opt = doctor.options();
+                    System.out.println(opt);
+                }
+                else if (cmd.equals("record")) {   // Deposit money
+                    doctor.record(args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
+                    System.out.println("You have successfully registered");
+                }
+//                else if (cmd.equals("withdraw")) {  // Withdraw money
+//                    FunnyMoney money = bank.withdraw(args[1], args[2],
+//						    Integer.parseInt(args[3]));
+//                    System.out.println("Withdrew " + money.amount +
+//				       " wooden nickels.");
+//                }
+                else if (cmd.equals("checkrecord")) {   // Check account balance
+                    String rec = doctor.checkRecord(args[1], args[2], args[3]);
+                    System.out.println("Your record:\n");
+                    System.out.println(rec + "\n");
+                }
+//                else if (cmd.equals("history")) {   // Get transaction history
+//                    List<String> transactions =
+//                    		bank.getTransactionHistory(args[1], args[2]);
+//                    for(int i = 0; i < transactions.size(); i++)
+//                        System.out.println(transactions.get(i));
+//                }
+                else System.out.println("Unknown command");
+            }
+            // Catch and display RMI exceptions
+            catch (RemoteException e) { System.err.println(e); }
+            // Catch and display Banking related exceptions
+            catch (RecordException e) { System.err.println(e.getMessage()); }
+            // Other exceptions are probably user syntax errors, so show usage.
+            catch (Exception e) { 
+                System.err.println(e);
+                System.err.println("Usage: java [-Ddoctor=<url>] Doctor$Client " +
+				   "<cmd> <surname> <name> <patronymic> [<other>]");
+                System.err.println("where cmd is: connect, disconnect, options, record, checkrecord");
+            }
+        }
+    }
+}
---
+++ Примеры/01/rmiBank/RemoteDoctorServer.java
@@ -0,0 +1,297 @@
+package rmiDoctor;
+
+import java.rmi.*;
+import java.rmi.server.*;
+import java.util.*;
+
+/**
+ * This class implements the remote methods defined by the RemoteBank
+ * interface.  It has a serious shortcoming, though: all account data is
+ * lost when the server goes down.
+ **/
+public class RemoteDoctorServer extends UnicastRemoteObject implements Doctor.RemoteDoctor
+{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -5298657303149865024L;
+
+	/** 
+     * This nested class stores data for a single account with the bank 
+     **/
+    class Account {
+        String userFullName;
+        String day = null;
+        String time = null;
+        String phone = null;
+        String complaint = null;
+
+        //String password;                      // account password
+        //int balance;                          // account balance
+        //List<String> transactions = new ArrayList<String>();  // account transaction history
+        Account(String surname, String name, String patronymic) {
+            this.userFullName = surname + name + patronymic;
+            //transactions.add("Account opened at " + new Date());
+        }
+    }
+    
+    /** 
+     * This hashtable stores all open accounts and maps from account name
+     * to Account object
+     **/
+    Map<String,Account> accounts = new HashMap<String,Account>();
+
+    private static int MAX_USERS = 100;
+
+    private static final int DAYS_SIZE = 6;
+    private static final int TIMES_SIZE = 9;
+
+    static TreeMap<String,Integer> daysToInt = new TreeMap<String,Integer>();
+    static {
+        daysToInt.put("Monday", 0);
+        daysToInt.put("Tuesday", 1);
+        daysToInt.put("Wednesday", 2);
+        daysToInt.put("Thursday", 3);
+        daysToInt.put("Friday", 4);
+        daysToInt.put("Saturday", 5);
+    }
+
+    static String[] daysToStr = new String[DAYS_SIZE];
+    static {
+        daysToStr[0] = "Monday";
+        daysToStr[1] = "Tuesday";
+        daysToStr[2] = "Wednesday";
+        daysToStr[3] = "Thursday";
+        daysToStr[4] = "Friday";
+        daysToStr[5] = "Saturday";
+    }
+
+    static TreeMap<String,Integer> timesToInt = new TreeMap<String,Integer>();
+    static {
+        timesToInt.put("10.00", 0);
+        timesToInt.put("11.00", 1);
+        timesToInt.put("12.00", 2);
+        timesToInt.put("13.00", 3);
+        timesToInt.put("14.00", 4);
+        timesToInt.put("15.00", 5);
+        timesToInt.put("16.00", 6);
+        timesToInt.put("17.00", 7);
+        timesToInt.put("18.00", 8);
+    }
+
+    static String[] timesToStr = new String[TIMES_SIZE];
+    static {
+        timesToStr[0] = "10.00";
+        timesToStr[1] = "11.00";
+        timesToStr[2] ="12.00";
+        timesToStr[3] = "13.00";
+        timesToStr[4] = "14.00";
+        timesToStr[5] = "15.00";
+        timesToStr[6] ="16.00";
+        timesToStr[7] = "17.00";
+        timesToStr[8] = "18.00";
+    }
+
+    static Account records[][] = new Account[DAYS_SIZE][TIMES_SIZE];
+    static
+    {
+        for (int i = 0; i < DAYS_SIZE; i++)
+        {
+            for (int j = 0; j < TIMES_SIZE; j++)
+            {
+                records[i][j] = null;
+            }
+        }
+    }
+    
+    /**
+     * This constructor doesn't do anything, but because the superclass 
+     * constructor throws an exception, the exception must be declared here
+     **/
+    public RemoteDoctorServer() throws RemoteException { super(); }
+    
+    /** 
+     * Open a bank account with the specified name and password 
+     * This method is synchronized to make it thread safe, since it 
+     * manipulates the accounts hashtable.
+     **/
+    public synchronized void connect(String surname, String name, String patronymic)
+	throws RemoteException, Doctor.RecordException
+    {
+        // Check if there is already an account under that name
+        if (accounts.get(surname + name + patronymic) != null)
+            throw new Doctor.RecordException("Account already exists.");
+        // Otherwise, it doesn't exist, so create it.
+        Account acct = new Account(surname, name, patronymic);
+        // And register it
+        accounts.put(acct.userFullName, acct);
+        System.out.print("Patient " + acct.userFullName + " is registered");
+    }
+    
+    /**
+     * This internal method is not a remote method.  Given a name and password
+     * it checks to see if an account with that name and password exists.  If
+     * so, it returns the Account object.  Otherwise, it throws an exception.
+     **/
+    Account verify(String surname, String name, String patronymic) throws Doctor.RecordException {
+        synchronized(accounts) {
+            Account acct = (Account)accounts.get(surname + name + patronymic);
+            if (acct == null) throw new Doctor.RecordException("No such account");
+            return acct;
+        }
+    }
+    
+    /** 
+     * Close the named account.  This method is synchronized to make it 
+     * thread safe, since it manipulates the accounts hashtable.
+     **/
+    public synchronized void disconnect(String surname, String name, String patronymic)
+	throws RemoteException, Doctor.RecordException
+    {
+        Account acct;
+        acct = verify(surname, name, patronymic);
+        accounts.remove(acct.userFullName);
+        System.out.print("Patient " + acct.userFullName + " disconnected");
+        // Before changing the balance or transactions of any account, we first
+        // have to obtain a lock on that account to be thread safe.
+        synchronized (acct) {
+            if (acct.day != null && acct.time != null)
+            {
+                records[daysToInt.get(acct.day)][timesToInt.get(acct.time)] = null;
+            }
+        }
+    }
+
+    public static boolean isValidDay(String d)
+    {
+        if (d.equals("Monday") || d.equals("Tuesday") || d.equals("Wednesday") || d.equals("Thursday") ||
+                d.equals("Friday") || d.equals("Saturday"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isValidTime(String t)
+    {
+        if (t.equals("10.00") || t.equals("11.00") || t.equals("12.00") || t.equals("13.00") ||
+                t.equals("14.00") || t.equals("15.00") || t.equals("16.00") || t.equals("17.00") || t.equals("18.00"))
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    /** Deposit the specified FunnyMoney to the named account */
+    public void record(String surname, String name, String patronymic, String day,
+                       String time, String phone, String complaint)
+	throws RemoteException, Doctor.RecordException
+    {
+        Account acct = verify(surname, name, patronymic);
+        synchronized(acct) {
+            if (!isValidDay(day) || !isValidTime(time))
+            {
+                throw new Doctor.RecordException("Incorrect day or(and) time type");
+            }
+            if (records[daysToInt.get(day)][timesToInt.get(time)] != null)
+            {
+                throw new Doctor.RecordException("This time is not available, try another option");
+            }
+            acct.day = day;
+            acct.time = time;
+            acct.complaint = complaint;
+            acct.phone = phone;
+            records[daysToInt.get(day)][timesToInt.get(time)] = acct;
+            System.out.print("Patient " + acct.userFullName + " registered:");
+            System.out.print("Day: " + acct.day + " time: " + acct.time + " phone: " + acct.phone);
+            System.out.print("Complaint: " + acct.complaint);
+        }
+    }
+    
+    /** Withdraw the specified amount from the named account */
+//    public Doctor.FunnyMoney withdraw(String name, String password, int amount)
+//	throws RemoteException, Doctor.RecordException
+//    {
+//        Account acct = verify(name, password);
+//        synchronized(acct) {
+//            if (acct.balance < amount)
+//                throw new Doctor.RecordException("Insufficient Funds");
+//            acct.balance -= amount;
+//            acct.transactions.add("Withdrew " + amount + " on "+new Date());
+//            return new Doctor.FunnyMoney(amount);
+//        }
+//    }
+    
+    /** Return the current balance in the named account */
+    public String checkRecord(String surname, String name, String patronymic)
+	throws RemoteException, Doctor.RecordException
+    {
+        Account acct = verify(surname, name, patronymic);
+        synchronized(acct)
+        {
+            String rec = "Day : " + acct.day + "\n"
+                    + "Time : " + acct.time + "\n"
+                    + "Phone : " + acct.phone + "\n"
+                    + "Complaint : " + acct.complaint + "\n";
+            return rec;
+        }
+    }
+
+    public String options()
+    {
+        Vector<String> opt = new Vector<String>();
+        for (int i = 0; i < DAYS_SIZE; i++)
+        {
+            for (int j = 0; j < TIMES_SIZE; j++)
+            {
+                if (records[i][j] == null)
+                {
+                    opt.add(daysToStr[i] + " " + timesToStr[j]);
+                }
+            }
+        }
+        StringBuilder optlist = new StringBuilder();
+        for (String str: opt) {
+            optlist.append("\n").append(str);
+        }
+        String strlist = optlist.toString();
+        return strlist;
+    }
+    
+    /** 
+     * Return a Vector of strings containing the transaction history
+     * for the named account
+     **/
+//    public List<String> getTransactionHistory(String name, String password)
+//	throws RemoteException, Doctor.RecordException
+//    {
+//        Account acct = verify(name, password);
+//        synchronized(acct) { return acct.transactions; }
+//    }
+    
+    /**
+     * The main program that runs this RemoteBankServer.
+     * Create a RemoteBankServer object and give it a name in the registry.
+     * Read a system property to determine the name, but use "FirstRemote"
+     * as the default name.  This is all that is necessary to set up the
+     * service.  RMI takes care of the rest.
+     **/
+    public static void main(String[] args) {
+        try {
+            // Create a bank server object
+            RemoteDoctorServer doctor = new RemoteDoctorServer();
+            // Figure out what to name it
+            String name = System.getProperty("doctorname", "FirstRemote");
+            // Name it that
+            Naming.rebind(name, doctor);
+            // Tell the world we're up and running
+            System.out.println(name + " is open and ready for patients.");
+        }
+        catch (Exception e) {
+            System.err.println(e);
+            System.err.println("Usage: java [-Ddoctorname=<name>] " +
+		            "RemoteDoctorServer");
+            System.exit(1); // Force exit because there may be RMI threads
+        }
+    }
+}
